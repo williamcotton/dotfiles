ef36ce871b38cdd33c1fa20d06ff9ca14933c4ac
William Cotton <williamc@eurekasoft.com>
2023-10-29 08:38:44 -0500

    modify test_plt.sh tests

--- bin/test_plt.sh
+++ bin/test_plt.sh
@@ -11,18 +11,18 @@ three, date {
   bar 10px dotted #7df
 }
 
-two, date {
+two,date {
   plot 10px solid #d83
 }
 
-A, date { highlight 0 1 solid yellow }
+A, date {highlight 0 1 solid yellow }
 
 two, date {
   plot 3px dotted green
 }
 
 [one, two, three], date { bar 10px [solid red, solid green, solid blue] }
-[one, two, three], date { stackbar 10px [solid orange, dashed #fed, dotted #8d2] }
+[one,two,three],date{stackbar 10px [solid orange,dashed #fed,dotted #8d2] }
 
 three, date { bleep blop blip green 10 }
 " <test.csv | imgc
