a9327c834136f874eceb35dbd6a5e29c729a08d1
William Cotton <williamc@eurekasoft.com>
2024-01-18 14:18:26 -0600

    add mysqlQuery

--- fsharp/DatabaseUtils.fsx
+++ fsharp/DatabaseUtils.fsx
@@ -2,10 +2,12 @@ module DatabaseUtils
 
 #r "nuget: Npgsql"
 #r "nuget: Newtonsoft.Json"
+#r "nuget: MySql.Data"
 
 open Npgsql
 open System.Collections.Generic
 open Newtonsoft.Json
+open MySql.Data.MySqlClient
 
 let connectToDatabaseBase connectionString =
     let connection = new NpgsqlConnection(connectionString)
@@ -47,3 +49,34 @@ let convertToJson results =
     match results with
     | Ok r -> Ok (convertToJsonBase r)
     | Error e -> Error e
+
+// A utility function to execute a database query and return results
+let mysqlQuery (connection: MySqlConnection) (query: string) (parameters: (string * obj) list) : Option<(string * obj) list> =
+    try
+        // Ensure the connection is open
+        if connection.State <> System.Data.ConnectionState.Open then
+            connection.Open()
+
+        // Create and prepare a command
+        using (new MySqlCommand(query, connection)) <| fun command ->
+            // Add parameters to the command
+            parameters |> List.iter (fun (key, value) ->
+                command.Parameters.AddWithValue(key, value) |> ignore)
+
+            // Execute the command and process the results
+            using (command.ExecuteReader()) <| fun reader ->
+                if reader.Read() then
+                    // Return results as an option of list of tuples with column name and value
+                    Some [ for i in 0 .. reader.FieldCount - 1 -> (reader.GetName(i), reader.GetValue(i)) ]
+                else
+                    // Return None if no result
+                    None
+    with
+    | :? MySqlException as ex ->
+        // Log MySql exceptions
+        printfn "MySql error: %s" ex.Message
+        None
+    | ex ->
+        // Log other exceptions
+        printfn "Unexpected error: %s" ex.Message
+        None
