7299cbebbc4db6a6e9cd267f462f50b17e2a4fc9
William Cotton <williamcotton@gmail.com>
2024-03-03 16:52:58 -0600

    add csvToExcel

--- fsharp/Excel.fsx
+++ fsharp/Excel.fsx
@@ -2,12 +2,14 @@ module Excel
 
 #r "nuget: ExcelDataReader"
 #r "nuget: ExcelDataReader.DataSet"
+#r "nuget: FsExcel"
 #r "nuget: CsvHelper"
 
 open System.IO
 open ExcelDataReader
 open System.Text
 open CsvHelper
+open FsExcel
 open System.Globalization
 
 let excelToCsv (excelFilePath: string) =
@@ -26,3 +28,36 @@ let excelToCsv (excelFilePath: string) =
         csvWriter.NextRecord() |> ignore
 
     Ok (csvStringBuilder.ToString())
+
+let csvToExcel (csvFilePath: string, excelFilePath: string) =
+    // Create the Excel file
+    use spreadsheetDocument = SpreadsheetDocument.Create(excelFilePath, SpreadsheetDocumentType.Workbook)
+    let workbookPart = spreadsheetDocument.AddWorkbookPart()
+    workbookPart.Workbook <- new Workbook()
+    let worksheetPart = workbookPart.AddNewPart<WorksheetPart>()
+    worksheetPart.Worksheet <- new Worksheet(new SheetData())
+    let sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets())
+
+    // Create a sheet
+    let sheet = new Sheet { Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet 1" }
+    sheets.Append(sheet)
+    workbookPart.Workbook.Save()
+
+    let sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>()
+
+    // Read CSV file and write to Excel
+    use reader = new StreamReader(csvFilePath)
+    use csvReader = new CsvReader(reader, CultureInfo.InvariantCulture)
+
+    let records = csvReader.GetRecords<dynamic>() |> Seq.toList
+    for record in records do
+        let row = new Row()
+        sheetData.AppendChild(row)
+        for (field, _) in record do
+            let cell = new Cell { CellValue = new CellValue(field.ToString()), DataType = CellValues.String }
+            row.AppendChild(cell)
+
+    // Save the changes to the worksheet part
+    worksheetPart.Worksheet.Save()
+
+    Ok "CSV converted to Excel successfully"
