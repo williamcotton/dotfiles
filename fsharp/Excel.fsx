ad6ded431bebd23918c49fac9b018a91ad279a9c
William Cotton <williamcotton@gmail.com>
2024-03-03 16:05:42 -0600

    add Excel helpers to F# libs

---
+++ fsharp/Excel.fsx
@@ -0,0 +1,28 @@
+module Excel
+
+#r "nuget: ExcelDataReader"
+#r "nuget: ExcelDataReader.DataSet"
+#r "nuget: CsvHelper"
+
+open System.IO
+open ExcelDataReader
+open System.Text
+open CsvHelper
+open System.Globalization
+
+let excelToCsv (excelFilePath: string) =
+    use stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read)
+    use reader = ExcelReaderFactory.CreateReader(stream)
+
+    let dataSet = reader.AsDataSet()
+    let table = dataSet.Tables.[0]
+
+    let csvStringBuilder = StringBuilder()
+    use csvWriter = new CsvWriter(new StringWriter(csvStringBuilder), CultureInfo.InvariantCulture)
+
+    for row in table.Rows do
+        for i in 0 .. table.Columns.Count - 1 do
+            csvWriter.WriteField(row.[i].ToString()) |> ignore
+        csvWriter.NextRecord() |> ignore
+
+    Ok (csvStringBuilder.ToString())
--- fsharp/Utils.fsx
+++ fsharp/Utils.fsx
@@ -2,8 +2,10 @@
 #load "Unix.fsx"
 #load "DatabaseUtils.fsx"
 #load "WebUtils.fsx"
+#load "Excel.fsx"
 
 open Jupyter
 open Unix
 open DatabaseUtils
 open WebUtils
+open Excel
