0a596be3a241394a6f2a0722519b403961f87318
William Cotton <williamc@eurekasoft.com>
2024-03-03 20:01:20 -0600

    minor updates to F# scripts

--- fsharp/Utils.fsx
+++ fsharp/Utils.fsx
@@ -1,11 +1,11 @@
+#load "DatabaseUtils.fsx"
 #load "Jupyter.fsx"
 #load "Unix.fsx"
-#load "DatabaseUtils.fsx"
 #load "WebUtils.fsx"
 #load "Excel.fsx"
 
+open DatabaseUtils
 open Jupyter
 open Unix
-open DatabaseUtils
 open WebUtils
 open Excel
