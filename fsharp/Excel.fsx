da950fac46be49e83a7a03cd47b904249d0ccdd7
William Cotton <williamc@eurekasoft.com>
2024-03-03 18:16:09 -0600

    remove non-functioning csvToExcel

--- fsharp/Excel.fsx
+++ fsharp/Excel.fsx
@@ -28,36 +28,3 @@ let excelToCsv (excelFilePath: string) =
         csvWriter.NextRecord() |> ignore
 
     Ok (csvStringBuilder.ToString())
-
-let csvToExcel (csvFilePath: string, excelFilePath: string) =
-    // Create the Excel file
-    use spreadsheetDocument = SpreadsheetDocument.Create(excelFilePath, SpreadsheetDocumentType.Workbook)
-    let workbookPart = spreadsheetDocument.AddWorkbookPart()
-    workbookPart.Workbook <- new Workbook()
-    let worksheetPart = workbookPart.AddNewPart<WorksheetPart>()
-    worksheetPart.Worksheet <- new Worksheet(new SheetData())
-    let sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets())
-
-    // Create a sheet
-    let sheet = new Sheet { Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet 1" }
-    sheets.Append(sheet)
-    workbookPart.Workbook.Save()
-
-    let sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>()
-
-    // Read CSV file and write to Excel
-    use reader = new StreamReader(csvFilePath)
-    use csvReader = new CsvReader(reader, CultureInfo.InvariantCulture)
-
-    let records = csvReader.GetRecords<dynamic>() |> Seq.toList
-    for record in records do
-        let row = new Row()
-        sheetData.AppendChild(row)
-        for (field, _) in record do
-            let cell = new Cell { CellValue = new CellValue(field.ToString()), DataType = CellValues.String }
-            row.AppendChild(cell)
-
-    // Save the changes to the worksheet part
-    worksheetPart.Worksheet.Save()
-
-    Ok "CSV converted to Excel successfully"
